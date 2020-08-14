652593e4bda7331af8f920c3cb274eef8c8b91e9
Facundo Baquero <facundito.xB@gmail.com>
2020-08-14 20:20:17 -0300

    cambios de nombre
    Index ahora es page
    Login ahora es index

--- js/login.js
+++ js/login.js
@@ -1,3 +1,23 @@
+const user = document.getElementById("user");
+const password = document.getElementById("contra");
+const form = document.getElementById("login");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    let users = Array (
+        {
+            usuario: user.value,
+            contraseña: password.value,
+        
+        }
+    );
+    localStorage.setItem('user', JSON.stringify(users));
+    location.href='page.html';     
+});
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
